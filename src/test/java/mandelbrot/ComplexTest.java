91801b83684f99fa4bdb952ed31f8d13872cdbe3
d16010869 <aureliendupuy5@gmail.com>
2018-11-14 11:06:07 +0100

    ajout testReal correction  Complex real

--- src/test/java/mandelbrot/ComplexTest.java
+++ src/test/java/mandelbrot/ComplexTest.java
@@ -133,4 +133,9 @@ public class ComplexTest {
         Complex c2 = new Complex(real, imaginary);
         assertEquals(c1.hashCode(), c2.hashCode());
     }
+
+    @Test
+    void testReal() {
+        assertEquals(new Complex(real, 0),Complex.real(real));
+    }
 }
